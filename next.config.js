a915832d2accac15ebf0d97d5caca0f8b34ac51f
mishab <mishabsanu298@gmail.com>
2026-05-13 14:20:24 +0530

    fet

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
         hostname: 'upload.wikimedia.org',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
     ],
   },
 };
